a8f05becbae2f8ecb22535fd718e2a3763bfbbae
gizn <github@georgnaver.se>
2025-08-01 08:02:09 +0200

    Search box styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import styles from "./page.module.css";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
