17f356193d76990a68e906faf6b7a63420cdba25
gizn <github@georgnaver.se>
2025-08-01 14:52:52 +0200

    MoList suspense tag

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,16 @@ import styles from "./page.module.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MoList from "./components/MoList";
+import { Suspense } from "react"
 
 export default function Home() {
   return (
     <div className={styles.page}>
     <Header />
       <main className={styles.main}>
-        <MoList />
+        <Suspense>
+          <MoList />
+        </Suspense>
       </main>
     <Footer />
     </div>
