ea53f4709f7f0521ce78d7406cb0ac6970647346
gizn <github@georgnaver.se>
2025-08-01 14:49:59 +0200

    MoList

---
+++ app/components/MoList.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+
+interface Mo {
+  mo: string;
+  fullname: string;
+  kingdom: string;
+  rank: string;
+  oxygen_tolerance: string;
+}
+
+const MoList = () => {
+  const [mos, setMos] = useState<Mo[]>([]);
+  const [filteredMos, setFilteredMos] = useState<Mo[]>([]);
+  const searchParams = useSearchParams();
+  const query = searchParams.get('query')?.toLowerCase() || '';
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('https://msberends.r-universe.dev/AMR/data/microorganisms/json');
+      const data: Mo[] = await res.json();
+      setMos(data);
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (query.length >= 3) {
+      // Normalize the query by removing non-alphanumeric characters and splitting into words
+      const queryWords = query
+        .toLowerCase()
+        .replace(/[^\w\s]/g, '') // Remove non-alphanumeric characters (punctuation)
+        .split(' ')
+        .filter(Boolean); // Split query into words and remove empty strings
+  
+      setFilteredMos(
+        mos.filter((mo) =>
+          mo.kingdom === 'Bacteria' && mo.rank === 'species' && queryWords.every((word) =>
+            mo.fullname.toLowerCase().replace(/[^\w\s]/g, '').includes(word) // Match after removing punctuation
+          )
+        )
+      );
+    } else {
+      setFilteredMos([]); // No microorganisms shown if query length is less than 3
+    }
+  }, [mos, query]);
+
+  return (
+    <>
+        <ul>
+            {filteredMos.map(mo => (
+                <li key={mo.mo}>{mo.fullname}</li>
+            ))}
+        </ul>
+    </>
+  );
+};
+
+export default MoList;
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import styles from "./page.module.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import MoList from "./components/MoList";
 
 export default function Home() {
   return (
     <div className={styles.page}>
     <Header />
       <main className={styles.main}>
-        <h1>Test</h1>
+        <MoList />
       </main>
     <Footer />
     </div>
