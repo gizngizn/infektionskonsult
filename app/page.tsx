c9a0b9d02018247b6dee38ce422a5d06993b4676
gizn <github@georgnaver.se>
2025-08-01 07:59:27 +0200

    Search box styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import styles from "./page.module.css";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div className={styles.page}>
     <Header />
       <main className={styles.main}>
-        <h1>Main</h1>
+        <h1>Test</h1>
       </main>
     <Footer />
     </div>
